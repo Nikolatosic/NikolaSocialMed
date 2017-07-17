6de4bc0883cc5990c79bbfabc1a22521750c1a3f
Nikola Tosic <nikola.tosic993@gmail.com>
2017-07-17 10:30:55 +0200

    Sign in ui custom classes complete

---
+++ NikolaSocialMed/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  NikolaSocialMed
+//
+//  Created by Nikola Tosic on 7/17/17.
+//  Copyright © 2017 Nikola Tosic. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
